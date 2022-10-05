854ac8a1eaa425510d66f3045fbe8b725395e5f5
gkobetic <gkobetic@gmail.com>
2022-10-05 14:35:14 +0200

    update

---
+++ SerialArduToArdu.cpp
@@ -0,0 +1,57 @@
+/*
+  SOURCE FILE
+  Gregor's library for "no stop" delay.
+  September 2022.
+*/
+
+#include "arduino.h"
+#include "MyDelay.h"
+
+//Constructor
+MyDelay::MyDelay(unsigned long milliseconds) {
+	delaytime = milliseconds; //Set input parameter in private variable
+	preMills = millis();
+	canExecute = false; //Parameter for executing the time interval
+}
+
+//Main logic
+bool MyDelay::isInterval() {
+	if (!canExecute) return false; //If this parameter is false than we don't execute the code
+
+	//Calcualte when time interval expires
+	curMills = millis();
+	if (preMills > curMills) preMills = 0; //Safety if millis() overflows
+	if (curMills - preMills >= delaytime) {
+		preMills = curMills;
+        return true;
+	}
+	else {
+        return false;
+	}		
+}
+
+//Method to start calculating intervals. This method resets preMills
+void MyDelay::start() {
+	canExecute = true;
+	preMills = millis();
+}
+
+//Method for stop calculating interval
+void MyDelay::stop() {
+	canExecute = false;
+}
+
+//Method resumes calcualtion of interval and doesn't reset preMills variable as start method does
+void MyDelay::resume() {
+	canExecute = true;
+}
+
+//Method resets preMills variable
+void MyDelay::reset() {
+	preMills = millis();
+}
+
+//Method for change the interval time
+void MyDelay::setInterval(unsigned long milliseconds) {
+	delaytime = milliseconds;
+}
---
+++ SerialArduToArdu.h
@@ -0,0 +1,32 @@
+/*
+  HEADER FILE
+  Gregor's library for "no stop" delay.
+  September 2022.
+*/
+
+#ifndef MyDelay_h
+#define MyDelay_h
+
+#include "Arduino.h"
+
+//Class definition
+class MyDelay
+{
+ 	public:
+	//Constructor
+	MyDelay(unsigned long milliseconds);
+	//Public methods and variables
+	bool isInterval();
+	void start();
+	void stop();
+	void resume();
+	void reset();
+	void setInterval(unsigned long milliseconds);
+	
+	private:
+	//Private methods and variables
+	unsigned long preMills, curMills, delaytime;
+	bool canExecute;
+};
+
+#endif
